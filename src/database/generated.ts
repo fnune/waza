e4c1b7bd533ef7df1e07a863ee51bbcc9ad4ad4f
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-10 19:58:25 +0100

    Add a lot of data, probably some of it wrong

--- src/database/generated.ts
+++ src/database/generated.ts
@@ -4,12 +4,45 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
 
+export type Json = JsonValue;
+
+export type JsonArray = JsonValue[];
+
+export type JsonObject = {
+  [K in string]?: JsonValue;
+};
+
+export type JsonPrimitive = boolean | number | string | null;
+
+export type JsonValue = JsonArray | JsonObject | JsonPrimitive;
+
+export interface Categories {
+  japanese: string;
+  parent: string | null;
+  romaji: string;
+  words_japanese: Json;
+  words_romaji: Json;
+}
+
 export interface Waza {
+  categories: Generated<Json>;
   id: Generated<number>;
-  name: string;
+  japanese: string;
+  romaji: string;
+  video_links: Generated<Json>;
+  words_japanese: Json;
+  words_romaji: Json;
+}
+
+export interface Words {
+  description: string | null;
+  english: string;
+  japanese: string;
   romaji: string;
 }
 
 export interface DB {
+  categories: Categories;
   waza: Waza;
+  words: Words;
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import { useLiveQuery } from "@electric-sql/pglite-react";
-
 import { useQuery } from "../services/database";
 
 export function Main() {
   const version = useLiveQuery("SELECT version();");
 
   const { value: waza } = useQuery((db) =>
-    db.selectFrom("waza").select(["id", "name", "romaji"]).execute(),
+    db
+      .selectFrom("waza")
+      .select(["id", "japanese", "romaji", "video_links", "categories"])
+      .execute(),
   );
 
   return (
@@ -14,9 +16,22 @@ export function Main() {
       <h1>Waza</h1>
       <p>A learning resource for judo practitioners.</p>
       <h2>Techniques</h2>
-      <pre>
-        <code>{JSON.stringify(waza, null, 2)}</code>
-      </pre>
+      {!waza && <p>Loading...</p>}
+      {waza && (
+        <ul>
+          {waza.map(({ id, japanese, romaji, ...rest }) => {
+            const categories = rest.categories as string[];
+            return (
+              <li key={id}>
+                <h3>
+                  {japanese} ({romaji})
+                </h3>
+                <p>{categories.join(", ")}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
       <h2>Health</h2>
       <pre>
         <code>{JSON.stringify(version, null, 2)}</code>
