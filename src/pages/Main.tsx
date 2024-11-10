f8daef53d2e1c975ee43a6fa3b8324c2212c5a72
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-10 19:59:28 +0100

    Show waza count

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -15,22 +15,24 @@ export function Main() {
     <>
       <h1>Waza</h1>
       <p>A learning resource for judo practitioners.</p>
-      <h2>Techniques</h2>
       {!waza && <p>Loading...</p>}
       {waza && (
-        <ul>
-          {waza.map(({ id, japanese, romaji, ...rest }) => {
-            const categories = rest.categories as string[];
-            return (
-              <li key={id}>
-                <h3>
-                  {japanese} ({romaji})
-                </h3>
-                <p>{categories.join(", ")}</p>
-              </li>
-            );
-          })}
-        </ul>
+        <>
+          <h2>Techniques ({waza.length})</h2>
+          <ul>
+            {waza.map(({ id, japanese, romaji, ...rest }) => {
+              const categories = rest.categories as string[];
+              return (
+                <li key={id}>
+                  <h3>
+                    {japanese} ({romaji})
+                  </h3>
+                  <p>{categories.join(", ")}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </>
       )}
       <h2>Health</h2>
       <pre>
