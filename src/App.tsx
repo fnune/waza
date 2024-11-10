5214c3cc1cfad6a0f2c9735951159d6b7367c13c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-10 12:03:26 +0100

    Add pglite

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { PGlite } from "@electric-sql/pglite";
+import { PGliteProvider } from "@electric-sql/pglite-react";
+import { live } from "@electric-sql/pglite/live";
+
+const db = await PGlite.create({
+  extensions: { live },
+});
+
 function App() {
   return (
-    <main>
-      <h1>Waza</h1>
-      <p>A learning resource for judo practitioners.</p>
-    </main>
+    <PGliteProvider db={db}>
+      <main>
+        <h1>Waza</h1>
+        <p>A learning resource for judo practitioners.</p>
+      </main>
+    </PGliteProvider>
   );
 }
 
