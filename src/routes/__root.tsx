8f19c1dfe0f538219372b1128508f4100758309d
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-17 19:30:58 +0100

    Avoid having the page look collapsed when first loading

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -26,7 +26,7 @@ function Main() {
 
   return (
     <I18nProvider locale={defaultLocale}>
-      <main className="bg-gradient-to-t from-neutral-300 to-stone-400 p-16 print:p-0">
+      <main className="min-h-dvh bg-gradient-to-t from-neutral-300 to-stone-400 p-16 print:p-0">
         <LanguageSelect />
         <Outlet />
         <div className="my-16" />
