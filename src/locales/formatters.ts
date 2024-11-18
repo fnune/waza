5ce034047d8760daa64e7d2705be9e7de49f7f74
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-18 21:05:11 +0100

    Add gh-pages

--- src/locales/formatters.ts
+++ src/locales/formatters.ts
@@ -1,11 +1,7 @@
-import type { FormattersInitializer } from 'typesafe-i18n'
-import type { Locales, Formatters } from './i18n-types'
+import type { FormattersInitializer } from "typesafe-i18n";
+import type { Formatters, Locales } from "./i18n-types";
 
-export const initFormatters: FormattersInitializer<Locales, Formatters> = (locale: Locales) => {
-
-	const formatters: Formatters = {
-		// add your formatter functions here
-	}
-
-	return formatters
-}
+export const initFormatters: FormattersInitializer<Locales, Formatters> = (_locale: Locales) => {
+  const formatters: Formatters = {};
+  return formatters;
+};
