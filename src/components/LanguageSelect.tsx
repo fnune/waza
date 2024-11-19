5e3c9a3230ea31f28fea3de6bc1b2e2406f0c0a1
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-11-19 08:55:02 +0100

    Make language select a bit more accessible

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -1,27 +1,32 @@
+import { useCallback } from "react";
 import { useI18nContext } from "~/locales/i18n-react";
-import { locales } from "~/locales/i18n-util";
+import type { Locales } from "~/locales/i18n-types";
+import type { locales } from "~/locales/i18n-util";
 
 export function LanguageSelect() {
   const { locale, setLocale } = useI18nContext();
   const sorted: typeof locales = ["en", "de", "es"];
-  const labels: Record<(typeof locales)[number], string> = {
+  const labels: Record<Locales, string> = {
     en: "English",
     de: "Deutsch",
     es: "Español",
   };
+
+  const onSetLocale = useCallback((lang: Locales) => () => {
+    setLocale(lang)
+  }, [setLocale])
+
   return (
     <ul className="flex justify-center gap-2 pb-6 print:hidden">
       {sorted.map((lang) => (
-        <li
+        <button
           key={lang}
-          className="cursor-pointer"
+          type="button"
           style={{ textDecoration: lang === locale ? "underline" : "none" }}
-          onClick={() => {
-            setLocale(lang);
-          }}
+          onClick={onSetLocale(lang)}
         >
           {labels[lang]}
-        </li>
+        </button>
       ))}
     </ul>
   );
