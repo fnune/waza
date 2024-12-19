b2ebfa889718db0cc07137f8758620de7285dbab
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-12-19 08:32:27 +0100

    Remove mentions of pre-commit, the project now uses Husky

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ Install Nix and `direnv` and run:
 ```sh
 cp .envrc.example .envrc
 direnv allow
-pre-commit install
 yarn
 yarn dev
 ```
--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
           biome
           nodejs
           ocamlPackages.cpdf
-          pre-commit
           yarn
         ];
       };
